2405d874366733bc6feab32cc8027b5b49ddae1f
ChandanaRondla <136729062+ChandanaRondla@users.noreply.github.com>
2025-05-14 13:42:31 -0700

    Update auth.service.js

--- health_guide_new/src/services/auth.service.js
+++ health_guide_new/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/accounts/';
+const API_URL = 'https://health-monitoring.onrender.com/accounts/';
 
 // Function to get a cookie by name
 export function getCookie(name) {
