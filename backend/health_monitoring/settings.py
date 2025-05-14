8c125fb7d3150c7ddfa209d75bd5695b4ebee3f7
ChandanaRondla <136729062+ChandanaRondla@users.noreply.github.com>
2025-05-14 03:09:20 -0700

    Update settings.py

--- backend/health_monitoring/settings.py
+++ backend/health_monitoring/settings.py
@@ -58,13 +58,14 @@ WSGI_APPLICATION = 'health_monitoring.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'healthdb',
-        'USER': 'himajasree',
-        'PASSWORD': 'himaja2k',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
