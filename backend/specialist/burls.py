dde19316324c65e76cda1c804c2298ec4b542a99
ChandanaRondla <136729062+ChandanaRondla@users.noreply.github.com>
2025-05-14 11:16:58 -0700

    Update burls.py

--- backend/specialist/burls.py
+++ backend/specialist/burls.py
@@ -1,7 +1,1 @@
-# backend/specialist/urls.py
-from django.urls import path
-from .views import SpecialistListView
-
-urlpatterns = [
-    path('api/specialists/', SpecialistListView.as_view(), name='specialist-list'),
-]
+ 
